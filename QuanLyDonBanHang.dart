7ad6be61abe0b51526dad1118d529c3411b404dd
Tu Kim Hoang <tukimhoang@MacBook-Pro.local>
2025-05-20 18:45:22 +0700

    QuanLySinhVien and QuanLyDonBanHang

---
+++ QuanLyDonBanHang.dart
@@ -0,0 +1,25 @@
+/*
+Bài tập quản lý đơn bán hàng
+Đề bài:
+Viết chương trình nhập vào thông tin của một hóa đơn bán hàng:
+● Tên sản phẩm.
+● Số lượng mua.
+● Đơn giá.
+Chương trình sẽ:
+1. Tính thành tiền = số lượng × đơn giá.
+2. Áp dụng giảm giá:
+○ Nếu thành tiền >= 1,000,000, giảm giá 10%.
+○ Nếu thành tiền từ 500,000 đến dưới 1,000,000, giảm giá 5%.
+○ Nếu dưới 500,000, không giảm giá.
+3. Tính thuế VAT 8% trên tổng số tiền sau giảm giá.
+4. In hóa đơn bao gồm:
+○ Tên sản phẩm, số lượng, đơn giá.
+○ Thành tiền, giảm giá, thuế VAT, và tổng thanh toán cuối cùng.
+ */
+import 'dart:io';
+
+void main(){
+  // Nhập thông tin của hóa đơn
+  print("Nhập tên sản phẩm: ");
+  
+}
