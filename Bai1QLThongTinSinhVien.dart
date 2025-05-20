be0120cac0598e41a8856537516ea3f3829948c2
Tu Kim Hoang <tukimhoang@MacBook-Pro.local>
2025-05-20 20:18:02 +0700

    update

---
+++ Bai1QLThongTinSinhVien.dart
@@ -0,0 +1,10 @@
+/*
+Bài 1: Quản lý thông tin sinh viên
+Yêu cầu:
+1.Tạo lớp Student với các thuộc tính: id, name, age, grade.
+2.Viết các phương thức:
+Nhập thông tin sinh viên.
+Hiển thị thông tin sinh viên.
+Viết chương trình quản lý danh sách sinh viên: thêm mới, hiển thị danh
+sách, và tìm kiếm sinh viên theo ID.
+ */
